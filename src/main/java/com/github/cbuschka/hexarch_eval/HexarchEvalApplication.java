b94ae641b621e1d6c6c06ff807820ee4f468fb1f
conni <cbuschka@gmail.com>
2020-12-29 20:02:50 +0100

    Project setup.

---
+++ src/main/java/com/github/cbuschka/hexarch_eval/HexarchEvalApplication.java
@@ -0,0 +1,13 @@
+package com.github.cbuschka.hexarch_eval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexarchEvalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexarchEvalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/cbuschka/hexarch_eval/HexarchEvalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.cbuschka.hexarch_eval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexarchEvalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
