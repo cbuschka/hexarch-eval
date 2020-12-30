816c52b6af5c327dfc86f698f4448c0309e9eb40
conni <cbuschka@gmail.com>
2020-12-30 17:49:16 +0100

    Tests now run with dynamic input.

--- webapp/src/test/java/com/github/cbuschka/hexarch_eval/inbound/mq/UpdateStockMessageReceiverIntegrationTest.java
+++ webapp/src/test/java/com/github/cbuschka/hexarch_eval/inbound/mq/UpdateStockMessageReceiverIntegrationTest.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Date;
+import java.util.Random;
+
+import static java.lang.Math.abs;
 
 @SpringBootTest
 class UpdateStockMessageReceiverIntegrationTest
@@ -15,7 +18,11 @@ class UpdateStockMessageReceiverIntegrationTest
 	@Test
 	public void shouldCreateUnknownStockEntry() throws Exception
 	{
-		UpdateStockMessage message = new UpdateStockMessage("s0", "i0", 1, new Date());
+		Random random = new Random();
+		String supplierNo = String.format("s%d", abs(random.nextInt()));
+		String itemNo = String.format("i#%s-%d", supplierNo, abs(random.nextInt()));
+
+		UpdateStockMessage message = new UpdateStockMessage(supplierNo, itemNo, 1, new Date());
 		updateStockMessageReceiver.onMessage(message);
 	}
 }
--- webapp/src/test/java/com/github/cbuschka/hexarch_eval/inbound/web/UpdateStockControllerIntegrationTest.java
+++ webapp/src/test/java/com/github/cbuschka/hexarch_eval/inbound/web/UpdateStockControllerIntegrationTest.java
@@ -11,6 +11,13 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
+import static java.lang.Math.abs;
+
 @SpringBootTest
 class UpdateStockControllerIntegrationTest
 {
@@ -30,11 +37,16 @@ class UpdateStockControllerIntegrationTest
 	@Test
 	public void shouldCreateUnknownStockEntry() throws Exception
 	{
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX");
+		Random random = new Random();
+		String supplierNo = String.format("s%d", abs(random.nextInt()));
+		String itemNo = String.format("i#%s-%d", supplierNo, abs(random.nextInt()));
+
 		this.mockMvc
 				.perform(
 						MockMvcRequestBuilders.post("/stockEntries")
 								.contentType(MediaType.APPLICATION_JSON)
-								.content("{\"supplierNo\": \"s0\", \"itemNo\":\"i0\",\"amount\":1,\"stockUpdatedAt\":\"2020-01-01T15:33+01:00\"}"))
+								.content(String.format("{\"supplierNo\": \"%s\", \"itemNo\":\"%s\",\"amount\":1,\"stockUpdatedAt\":\"%s\"}", supplierNo, itemNo, dateFormat.format(new Date()))))
 				.andExpect(MockMvcResultMatchers.status().isNoContent());
 	}
 }
